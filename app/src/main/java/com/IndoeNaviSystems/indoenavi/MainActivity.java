7f2d12ed7d9d736661a19a57b3b401e1c4635b08
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2023-03-14 10:13:28 +0100

    Added design for navigation view

--- app/src/main/java/com/IndoeNaviSystems/indoenavi/MainActivity.java
+++ app/src/main/java/com/IndoeNaviSystems/indoenavi/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Hide the action bar for more visual space.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_main);
 
     }

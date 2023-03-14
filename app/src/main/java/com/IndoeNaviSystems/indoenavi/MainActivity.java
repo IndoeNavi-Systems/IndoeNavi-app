93023bc109ead4f810ce42b5d2de329a674a39f3
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2023-03-14 13:28:50 +0100

    Added design for destination view

--- app/src/main/java/com/IndoeNaviSystems/indoenavi/MainActivity.java
+++ app/src/main/java/com/IndoeNaviSystems/indoenavi/MainActivity.java
@@ -2,7 +2,11 @@ package com.indoenavisystems.indoenavi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.destinationTextView);
+
+        Button stopBtn = (Button) findViewById(R.id.stopBtn);
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //When navigation stopped, go back to destination view.
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        String destination = intent.getStringExtra("destination");
+        tv.setText(destination);
 
     }
 }
